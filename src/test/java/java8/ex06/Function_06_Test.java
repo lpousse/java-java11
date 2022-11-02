024c69344a78d85d41141ae3b2e3a353b6853aaa
Lucas Pousse <lucas.pousse@outlook.fr>
2022-11-02 12:37:03 +0100

    TP03

--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -17,8 +17,7 @@ public class Function_06_Test {
     // TODO compléter la méthode
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
-        // TODO
-        return null;
+        return "[age=" + supplier.get().getAge() + "]";
     }
     // end::formatAge[]
 
@@ -26,7 +25,8 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+    	Supplier<Person> s = () -> new Person("Jean", "Jacques", 35, "mdp");
+        String result = formatAge(s);
 
         assert result.equals("[age=35]");
     }
